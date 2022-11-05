3262828c5a5aac339d99063a550f0a2dc01785aa
Barnabas Varga <vabarnabas@gmail.com>
2022-11-05 15:14:25 +0000

    Some Progress

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -3,9 +3,14 @@ import { HiPlusSm } from "react-icons/hi";
 
 const Navbar = () => {
   return (
-    <div className="fixed inset-x-0 top-0 flex h-14 items-center justify-between border-b px-6">
+    <div className="fixed inset-x-0 top-0 flex h-12 items-center justify-between border-b px-6">
       <p className="cursor-pointer font-bold">Trava</p>
-      <div className="flex items-center  text-2xl"></div>
+      <div className="flex items-center">
+        <button className="flex items-center rounded-md bg-slate-900 px-3 py-1 text-sm text-white outline-none hover:bg-slate-800">
+          <HiPlusSm className="mr-1" />
+          New
+        </button>
+      </div>
     </div>
   );
 };
